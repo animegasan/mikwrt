6ab4b95e5b1b0f1626d8346d4cd94e7d894ad689
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-22 13:13:35 +0700

    Add files via upload

---
+++ luci-app-adguardhome/luasrc/view/AdGuardHome/redirect.lua
@@ -0,0 +1,3 @@
+<%+cbi/valueheader%>
+<font id="AdGuardHome_status_redirect"><%=pcdata(self:cfgvalue(section) or self.default or "")%></font>
+<%+cbi/valuefooter%>
---
+++ luci-app-adguardhome/luasrc/view/AdGuardHome/service.lua
@@ -0,0 +1,3 @@
+<%+cbi/valueheader%>
+<font id="AdGuardHome_status_service"><%=pcdata(self:cfgvalue(section) or self.default or "")%></font>
+<%+cbi/valuefooter%>
