eaa629b1f4c9fdb8aceda2138bad172fdd3374ee
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-22 13:12:09 +0700

    Add files via upload

---
+++ luci-app-adguardhome/luasrc/view/AdGuardHome/index.lua
@@ -0,0 +1,23 @@
+<% include("cbi/map") %>
+<script type="text/javascript">//<![CDATA[
+XHR.poll(3, '<%=url([[admin]], [[services]], [[AdGuardHome]], [[status]])%>', null,
+	function(x, data) {
+		var service = document.getElementById('AdGuardHome_status_service');
+		var redirect = document.getElementById('AdGuardHome_status_redirect');
+		if (data && service && redirect) {
+			if (data.running) {
+				service.innerHTML = '<b style=color:green><%:Running%></b>';
+			} else {
+				service.innerHTML = '<b style=color:red><%:Stopped%></b>';
+			}
+			if (data.redirect)
+			{
+				redirect.innerHTML = '<b style=color:green><%:Redirected%></b>'
+			} else {
+				redirect.innerHTML = '<b style=color:red><%:Stopped%></b>'	
+			}
+		}
+	}
+);
+//]]>
+</script>
